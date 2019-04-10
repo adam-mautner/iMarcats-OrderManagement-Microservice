513b77c700a37b0479ae1e4381346ab4237f9852
Adam Mautner <mautner.adam@gmail.com>
2019-04-10 23:24:58 +0200

    Removing unnecessary items from order management

--- order-management-microservice/src/main/java/com/imarcats/microservice/order/management/OrderManagementSystemFactory.java
+++ order-management-microservice/src/main/java/com/imarcats/microservice/order/management/OrderManagementSystemFactory.java
@@ -21,14 +21,9 @@ import com.imarcats.internal.server.infrastructure.notification.trades.TradeNoti
 import com.imarcats.internal.server.infrastructure.notification.trades.TradeNotificationBrokerImpl;
 import com.imarcats.internal.server.infrastructure.notification.trades.TradeNotificationSession;
 import com.imarcats.internal.server.infrastructure.notification.trades.TradeNotificationSessionImpl;
-import com.imarcats.internal.server.interfaces.order.OrderInternal;
 import com.imarcats.internal.server.interfaces.order.OrderManagementContext;
 import com.imarcats.internal.server.interfaces.order.OrderManagementContextImpl;
-import com.imarcats.market.engine.matching.OrderCancelActionExecutor;
-import com.imarcats.market.engine.matching.OrderSubmitActionExecutor;
-import com.imarcats.market.engine.order.OrderCancelActionRequestor;
 import com.imarcats.market.engine.order.OrderManagementSystem;
-import com.imarcats.market.engine.order.OrderSubmitActionRequestor;
 import com.imarcats.microservice.order.management.notification.KafkaMessageBroker;
 import com.imarcats.microservice.order.management.notification.NotificationBrokerImpl;
 import com.imarcats.microservice.order.management.order.OrderActionRequestor;
@@ -57,34 +52,6 @@ public class OrderManagementSystemFactory {
 	@Bean
 	public OrderManagementSystem createOrderManagementSystem() {
 		return new OrderManagementSystem(marketDatastore, orderDatastore, tradeDatastore, orderActionRequestor, orderActionRequestor);
-	}
-	
-	private class MockOrderActionRequestor1 implements OrderSubmitActionRequestor, OrderCancelActionRequestor {
-		
-		private OrderSubmitActionExecutor _orderSubmitActionExecutor;
-		private OrderCancelActionExecutor _orderCancelActionExecutor;
-
-		public MockOrderActionRequestor1() {
-			super();
-			_orderSubmitActionExecutor = new OrderSubmitActionExecutor(marketDatastore, orderDatastore);
-			_orderCancelActionExecutor = new OrderCancelActionExecutor(marketDatastore, orderDatastore);
-		}
-		
-		@Override
-		public void cancelOrder(OrderInternal orderInternal_,
-				String cancellationCommentLanguageKey_,
-				OrderManagementContext orderManagementContext_) {
-			// TODO: Separate this action with Kafka communication 
-			_orderCancelActionExecutor.cancelOrder(orderInternal_.getKey(), cancellationCommentLanguageKey_, orderManagementContext_);
-		}
-
-		@Override
-		public void submitOrder(OrderInternal orderInternal_,
-				OrderManagementContext orderManagementContext_) {
-			// TODO: Separate this action with Kafka communication 
-			_orderSubmitActionExecutor.submitOrder(orderInternal_.getKey(), orderManagementContext_);
-		}
-
 	}
 	
 	@Bean
